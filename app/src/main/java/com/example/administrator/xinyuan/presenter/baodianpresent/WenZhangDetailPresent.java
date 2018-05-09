f5454f2db97e40e5a1f8e63d9b6ebc2d7f05b6ca
fghjkl <1040033126@qq.com>
2018-05-09 11:42:07 +0800

    111

--- app/src/main/java/com/example/administrator/xinyuan/presenter/baodianpresent/WenZhangDetailPresent.java
+++ app/src/main/java/com/example/administrator/xinyuan/presenter/baodianpresent/WenZhangDetailPresent.java
@@ -1,20 +1,16 @@
 package com.example.administrator.xinyuan.presenter.baodianpresent;
 
-import android.util.Log;
-
 import com.example.administrator.xinyuan.contact.baodiancontact.IWenZhangDetailContact;
 import com.example.administrator.xinyuan.model.biz.IHuoQuYZMaService;
+import com.example.administrator.xinyuan.model.entity.TieZiBean;
 import com.example.administrator.xinyuan.model.http.RetrofitUtils;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
-import okhttp3.ResponseBody;
 
 /**
  * Created by fghjkl on 2018/5/8.
@@ -36,30 +32,10 @@ public class WenZhangDetailPresent implements IWenZhangDetailContact.Present {
         iHuoQuYZMaService.loaddetail(params)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<ResponseBody>() {
+                .subscribe(new Consumer<TieZiBean>() {
                     @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(ResponseBody value) {
-                        try {
-                            String string = value.string();
-                            Log.e("cccccc",string);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
+                    public void accept(TieZiBean tieZiBean) throws Exception {
+                        view.showShuJu();
                     }
                 });
     }
